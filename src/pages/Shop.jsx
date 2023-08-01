ddd354825481404c5a088c17b2ad3ac1ed42fa47
phyrunez <ebukajudee302@gmail.com>
2023-08-01 20:52:16 +0100

    add filter and search functions

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,131 +1,149 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from "react";
 import { Container, Row, Col } from "reactstrap";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSections from "../components/UI/CommonSections";
 import "../styles/shop.css";
-import { toast } from 'react-toastify';
-import axios from "axios"
+import { toast } from "react-toastify";
+import axios from "axios";
 
 import products from "../assets/data/products";
 import ProductList from "../components/UI/ProductList";
 
 const Shop = () => {
-  const [productState, setProductState] = useState([])
-  const [loadingState, setLoadingState] = useState('Fetching Products...')
+	const [defaultProductState, setDefaultProductState] = useState([]);
+	const [productState, setProductState] = useState([]);
+	const [loadingState, setLoadingState] = useState("Fetching Products...");
 
-  useEffect(() => {
-    
-    const getData = async() => {
-      await axios.get('https://fakestoreapi.com/products')
-      .then(res => {
-        const prod = res?.data
-        console.log(prod)
-        setLoadingState('Loading')
-        setProductState(prod) 
-        setLoadingState('')
-      })
-    }
+	useEffect(() => {
+		const getData = async () => {
+			await axios.get("https://fakestoreapi.com/products").then((res) => {
+				const prod = res?.data;
+				console.log(prod);
+				setLoadingState("Loading");
+				setDefaultProductState(prod);
+				setProductState(prod);
+				setLoadingState("");
+			});
+		};
 
-    getData()
-  }, [])
+		getData();
+	}, []);
 
-  const handleFilter = (e) => {
+	const handleFilter = (e) => {
     const filterValue = e.target.value;
-    if(filterValue === "men's clothing") {
-      const filteredProducts = productState.filter(
-        item => item.category === "men's clothing"
-      );
-      setProductState(filteredProducts)
+    if (!filterValue) {
+      setProductState(defaultProductState);
     }
+		if (filterValue === "men's clothing") {
+			const filteredProducts = defaultProductState.filter(
+				(item) => item.category === "men's clothing",
+			);
+			setProductState(filteredProducts);
+		}
 
-    if(filterValue === "women's clothing") {
-      const filteredProducts = productState.filter(
-        item => item.category === "women's clothing"
-      );
-      setProductState(filteredProducts)
-    }
+		if (filterValue === "women's clothing") {
+			const filteredProducts = defaultProductState.filter(
+				(item) => item.category === "women's clothing",
+			);
+			setProductState(filteredProducts);
+		}
 
-    if(filterValue === "jewelery") {
-      const filteredProducts = productState.filter(
-        item => item.category === "jewelery"
-      );
-      setProductState(filteredProducts)
-    }
+		if (filterValue === "jewelery") {
+			const filteredProducts = defaultProductState.filter(
+				(item) => item.category === "jewelery",
+			);
+			setProductState(filteredProducts);
+		}
 
-    if(filterValue === "electronics") {
-      const filteredProducts = productState.filter(
-        item => item.category === "electronics"
-      );
-      setProductState(filteredProducts)
-    }
-  }
+		if (filterValue === "electronics") {
+			const filteredProducts = defaultProductState.filter(
+				(item) => item.category === "electronics",
+			);
+			setProductState(filteredProducts);
+		}
+	};
 
-  const handleSearch = e => {
-    const searchTerm = e.target.value;
-    const searchedProducts = productState.filter(item => item.title.toLowerCase().includes(searchTerm.toLowerCase()))
+	const handleSearch = (e) => {
+		const searchTerm = e.target.value;
+		if (searchTerm === "") return setProductState(defaultProductState);
+		const searchedProducts = defaultProductState.filter((item) =>
+			item.title.toLowerCase().includes(searchTerm.toLowerCase()),
+		);
 
-    setProductState(searchedProducts);
+		setProductState(searchedProducts);
+	};
 
-    if(searchTerm === '') return productState
-  }
+	const setOptionCategoryData = useMemo(() => {
+		const tracker = [];
+		const result = defaultProductState?.map((element) => {
+			const hasDuplicate = tracker.find((item) => item === element.category);
+			if (!hasDuplicate) {
+				tracker.push(element.category);
+				return element.category;
+			}
+			return undefined;
+		});
+		return result
+			.filter((item) => item)
+			.map((category) => (
+				<option value={category} key={category}>
+					{category}
+				</option>
+			));
+	}, [defaultProductState]);
 
-  const setOptionCategoryData = () => {
-    return productState.map(element => {
-      return <option value={element.id}>{ element.category }</option>
-    });
-    
-  }
+	return (
+		<Helmet title="shop">
+			<CommonSections title="Products" />
+			<section>
+				<Container>
+					<Row>
+						<Col lg="3" md="3">
+							<div className="filter__widget">
+								<select onChange={handleFilter}>
+									<option value="">Products Category</option>
+									{setOptionCategoryData}
+								</select>
+							</div>
+						</Col>
 
-  setOptionCategoryData()
+						<Col lg="6" md="6">
+							<div className="search__box">
+								<input
+									type="text"
+									placeholder="Search Products..."
+									onChange={handleSearch}
+								/>
+								<span>
+									<i className="ri-search-line"></i>
+								</span>
+							</div>
+						</Col>
 
-  return (
-    <Helmet title="shop">
-      <CommonSections title="Products" />
-      <section>
-        <Container>
-          <Row>
-            <Col lg="3" md="3">
-              <div className="filter__widget">
-                <select onChange={handleFilter}>
-                  <option value="">Products Category</option>
-                  { setOptionCategoryData }
-                </select>
-              </div>
-            </Col>
+						<Col lg="3" md="3">
+							<div className="filter__widget">
+								<button className="filter__widget__btn">
+									All Available Products
+								</button>
+							</div>
+						</Col>
+					</Row>
+				</Container>
+			</section>
 
-            <Col lg="6" md="6">
-              <div className="search__box">
-                <input 
-                  type="text" 
-                  placeholder="Search Products..." 
-                  onChange={handleSearch}
-                />
-                <span><i className="ri-search-line"></i></span>
-              </div>
-            </Col>
+			<section className="pt-0">
+				<Container>
+					<Row>
+						{productState.length === 0 ? (
+							<h1 className="text-center">{loadingState}</h1>
+						) : (
+							<ProductList data={productState} />
+						)}
+					</Row>
+				</Container>
+			</section>
+		</Helmet>
+	);
+};
 
-            <Col lg="3" md="3">
-              <div className="filter__widget">
-                <button className="filter__widget__btn">All Available Products</button>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </section>
-
-      <section className="pt-0">
-        <Container>
-          <Row>
-            {productState.length === 0 ? (
-              <h1 className="text-center">{ loadingState }</h1> 
-            ) : (
-              <ProductList data={productState} />
-            )}
-          </Row>
-        </Container>
-      </section>
-    </Helmet>
-  )
-}
-
-export default Shop
+export default Shop;
